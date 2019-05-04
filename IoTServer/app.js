c8cd1318c554fb7126f0b7a571a5a850ade0b5b2
Tkappa <igor.ershov97@gmail.com>
2019-05-04 13:02:22 +0200

    First test of server, limited functionalities

---
+++ IoTServer/app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+var path=require('path');
+
+var r=0;
+var b=0;
+var g=0;
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+	   extended: false
+}));
+
+app.listen(8000, function () {
+	  console.log('Example app listening on port 8000!');
+});
+
+
+app.get('/',function(req,res){
+	res.send("SP+"+r+";"+b+";"+g+";-E");
+	console.log("omg"+ new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds());
+});
+
+app.post('/postc',function(req,res){
+	var color=req.body.favcolor;
+	var hehe=hexToRgb(color);
+	console.log(hehe);
+	r=hehe.r;
+	g=hehe.g;
+	b=hehe.b;
+	res.redirect("/");
+});
+
+app.post('/hi',function(req,res){
+	var color=req.body.value;
+	console.log(req.body);
+	console.log(color);
+	res.sendStatus(200);
+});
+
+app.get('/color',function(req,res){
+	res.sendFile(path.join(__dirname+'/color.html'));
+});
+
+
+function rgbToHex(r, g, b) {
+	    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+
+function hexToRgb(hex) {
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+		            return r + r + g + g + b + b;
+		        });
+
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	    return result ? {
+		            r: parseInt(result[1], 16),
+		            g: parseInt(result[2], 16),
+		            b: parseInt(result[3], 16)
+		        } : null;
+}
